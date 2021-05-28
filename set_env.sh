872cde5f581290f87b894de7965678eb6b95a201
aymanshawky <aymanshawky2009@gmail.com>
2021-05-28 17:38:39 +0200

    test

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdb
+export POSTGRES_PASSWORD=udagramdb
+export POSTGRES_HOST=udagramdb.cfgpr6pmgykx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=my-1346455-bucket
+export AWS_REGION=us-west-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
